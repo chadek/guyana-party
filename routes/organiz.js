5916a9fbbf3bceab6161546116bd953592bde901
YohanPeslier <yohan.peslier.25@gmail.com>
2018-02-28 23:17:30 -0400

    adding user as admin and member during creation

--- routes/organiz.js
+++ routes/organiz.js
@@ -18,14 +18,16 @@ var gfs = "";
 
 db.on('error', console.error.bind(console, 'error while connecting to DB'));
 db.once('open',function(){
-  gfs = Grid(db.db);
-  console.log("connection to DB OK");
+	gfs = Grid(db.db);
+	console.log("connection to DB OK");
 });
 
 
 // require mongoose model (define in /model/models.js)
 var models = require( path.join(__dirname, '../model/models' ))(mongoose);
 var Organiz = models.Organiz;
+var Admin = models.Admin;
+var Member = models.Member;
 
 // set destination for uploaded flyer
 var upload = multer({ destination: '/logos' })
@@ -33,17 +35,18 @@ var upload = multer({ destination: '/logos' })
 
 /* GET users listing. */
 router.get('/creation', function(req, res, next){
-  if(req.isAuthenticated()){
-    res.render('creation_organization', {user: req.user});
-  } else {
-    res.redirect('/inscription');
-  }
+	if(req.isAuthenticated()){
+		res.render('creation_organization', {user: req.user});
+	} else {
+		res.redirect('/inscription');
+	}
 });
 
 /*----- GET REQUEST -----*/
 
 router.get('/id/:organizId', function(req, res, next){
 	console.log(req.params.organizId);
+	// Organiz.findOne({})
 	res.render('organization', {user: req.user, organiz: req.params.organizId});
 });
 
@@ -52,73 +55,107 @@ router.get('/id/:organizId', function(req, res, next){
 /*  ---------- POST REQUEST ----- */
 // Creat organiz with data send by user
 router.post('/creation/ajouter', upload.single('logo') ,function (req, res, next) {
-  console.log("ajout");
-  console.log(req.body);
-  console.log(req.file);
-  // if user is logged
-  if(req.user){
-    // if user send flyer, store it in db
-    if (req.file != undefined){
-      // streaming to gridfs
-      //filename to store in mongodb
-      var writestream = gfs.createWriteStream({
-          mode: 'w',
-          filename: req.file.originalname
-      });
-      streamifier.createReadStream(req.file.buffer).pipe(writestream);
+	console.log("ajout");
+	console.log(req.body);
+	console.log(req.file);
+	// if user is logged
+	if(req.user){
+		// if user send flyer, store it in db
+		if (req.file != undefined){
+			// streaming to gridfs
+			//filename to store in mongodb
+			var writestream = gfs.createWriteStream({
+					mode: 'w',
+					filename: req.file.originalname
+			});
+			streamifier.createReadStream(req.file.buffer).pipe(writestream);
 
-      writestream.on('close', function (file) {
-        // do something with `file`
-        console.log(file.filename + ' Written To DB');
+			writestream.on('close', function (file) {
+				// do something with `file`
+				console.log(file.filename + ' Written To DB');
 
-        var organiz = new Organiz({ 
-        						name: req.body.name,
-        						logo: file._id,
-        						type: req.type,
-        						description: req.body.description,
-        						location: { 
-									longitude: req.body.longitude,
-                        			latitude: req.body.latitude,
-                        			address: req.body.address
-                                },
-                                users:{ 
-                                	administrator: [req.user] },
-                                });
-        console.log("CREATING ORGANIZATION (with logo) :", req.body.name);
-        organiz.save(function(err, result) {
-          if (err) throw err;
-          console.log("ORGANIZATION CREATED");
-          res.redirect('/organization/id/'+ result._id);
-        });
-      });
-    } else {
+				var organiz = new Organiz({ 
+					name: req.body.name,
+					logo: file._id,
+					type: req.type,
+					description: req.body.description,
+					longitude: req.body.longitude,
+					latitude: req.body.latitude,
+					address: req.body.address
+				});
+
+				
+				console.log("CREATING ORGANIZATION (with logo) :", req.body.name);
+				organiz.save(function(err, result) {
+					if (err) throw err;
+					console.log("ORGANIZATION CREATED");
+					// is a admin too, so
+					var admin = new Admin({
+						userID: req.user,
+						organizId: result._id
+					});
+					console.log("Adding  user right !");
+					admin.save(function(err, result){
+						if (err) throw err;
+						console.log("Added as administrator");
+					});
+
+					var membre = new Member({
+						userID: req.user,
+						organizId: result._id
+					});
+					membre.save(function(err, result){
+						if (err) throw err;
+						console.log("Added as member too");
+					});
+
+					res.redirect('/organization/id/'+ result._id);
+				});
+			});
+		} else {
 
 
-      // if no logo, store null in flyer field
-      var organiz = new Organiz({ 
-        						name: req.body.name,
-        						logo: 'noLogo',
-        						type: req.type,
-        						description: req.body.description,
-        						location: { 
-									longitude: req.body.longitude,
-                        			latitude: req.body.latitude,
-                        			address: req.body.address
-                                },
-                                users:{ 
-                                	administrator: [req.user] },
-                                });
+			// if no logo, store null in flyer field
+			var organiz = new Organiz({ 
+				name: req.body.name,
+				logo: 'noLogo',
+				type: req.type,
+				description: req.body.description,
+				longitude: req.body.longitude,
+				latitude: req.body.latitude,
+				address: req.body.address
+			});
 
-      console.log("CREATING ORGANIZ (without logo) :", req.body.name);
-      organiz.save(function(err, result) {
-        if (err) throw err;
-        console.log("ORGANIZ CREATED");
-        res.redirect('/organization/id/'+ result._id);
-      });
-    }
-  }else {
-    res.redirect('/inscription');
-  }
+			console.log("CREATING ORGANIZ (without logo) :", req.body.name);
+			organiz.save(function(err, result) {
+				if (err) throw err;
+
+				console.log("ORGANIZ CREATED");
+				// is a admin too, so
+				var admin = new Admin({
+					userID: req.user,
+					organizId: result._id
+				});
+				console.log("Adding  user right !");
+				admin.save(function(err, result){
+					if (err) throw err;
+					console.log("Added as administrator");
+				});
+
+				var membre = new Member({
+					userID: req.user,
+					organizId: result._id
+				});
+				membre.save(function(err, result){
+					if (err) throw err;
+					console.log("Added as member too");
+				});
+				res.redirect('/organization/id/'+ result._id);
+			});
+		}
+	}else {
+		res.redirect('/inscription');
+	}
 });
 
 module.exports = router;
