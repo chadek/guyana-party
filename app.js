86d0b62ad27757a6f5ef72e3f6915af4eb0942fb
spidergon <spidergon@gmail.com>
2018-03-30 12:22:52 -0300

    restructure app.js

--- app.js
+++ app.js
@@ -1,58 +1,138 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var index = require( path.join(__dirname, './routes/index') );
-var users = require( path.join(__dirname, './routes/users') );
-var event = require( path.join(__dirname, './routes/event') );
-var organiz = require( path.join(__dirname, './routes/organiz') );
-var passport = require( path.join(__dirname, './Utils/auth') );
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const expressValidator = require("express-validator");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const mongoose = require("mongoose");
+const passport = require("passport");
+const flash = require("connect-flash");
+const helpers = require("./helpers");
+const { promisify } = require("es6-promisify");
+const index = require("./routes/index");
+//const users = require("./routes/users");
+//const events = require("./routes/events");
+//const organiz = require("./routes/organiz");
+const errorHandlers = require("./handlers/errorHandlers");
+require("./handlers/passport");
 
-var app = express();
+// const favicon = require('serve-favicon');
+// const logger = require('morgan');
+
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { }
-}));
+// Serves up static files from the public folder. Anything in public/ will just be served up as the file it is
+app.use(express.static(path.join(__dirname, "public")));
 
+// Takes the raw requests and turns them into usable properties on req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Exposes a bunch of methods for validating data. Used heavily on userController.validateRegister
+app.use(expressValidator());
+
+// populates req.cookies with any cookies that came along with the request
+app.use(cookieParser());
+
+// Sessions allow us to store data on visitors from request to request
+// This keeps users logged in and allows us to send flash messages
+app.use(
+  session({
+    secret: process.env.SECRET,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+  })
+);
+
+// Passport JS is what we use to handle our logins
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
-app.use('/evenement', event);
-app.use('/organization', organiz);
+// The flash middleware let's us use req.flash('error', 'Shit!'), which will then pass that message to the next page the user requests
+app.use(flash());
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// pass variables to our templates + all requests
+app.use((req, res, next) => {
+  res.locals.h = helpers;
+  res.locals.flashes = req.flash();
+  res.locals.user = req.user || null;
+  res.locals.currentPath = req.path;
+  next();
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// promisify some callback based APIs
+app.use((req, res, next) => {
+  //req.login = promisify(req.login, req);
+  next();
 });
 
+// After allllll that above middleware, we finally handle our own routes!
+app.use("/", index);
+// app.use("/users", users);
+// app.use("/evenement", events);
+// app.use("/organization", organiz);
+
+// If that above routes didnt work, we 404 them and forward to error handler
+app.use(errorHandlers.notFound);
+
+// One of our error handlers will see if these errors are just validation errors
+app.use(errorHandlers.flashValidationErrors);
+
+// Otherwise this was a really bad error we didn't expect! Shoot eh
+if (app.get("env") === "development") {
+  /* Development Error Handler - Prints stack trace */
+  app.use(errorHandlers.developmentErrors);
+}
+
+// production error handler
+app.use(errorHandlers.productionErrors);
+
+//--------------------------
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(session({
+//   secret: 'keyboard cat',
+//   resave: false,
+//   saveUninitialized: true,
+//   cookie: { }
+// }));
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', index);
+// app.use('/users', users);
+// app.use('/evenement', event);
+// app.use('/organization', organiz);
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+// done! we export it so we can start the site in start.js
 module.exports = app;
