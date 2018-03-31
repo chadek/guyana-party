7cf15930c482b21746d34e9ab51e2374f0e2fcdc
spidergon <spidergon@gmail.com>
2018-03-31 20:23:15 -0300

    events map

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -3,8 +3,10 @@ import "../sass/style.scss";
 import { S, SS } from "./modules/bling";
 import { loginForm } from "./modules/loginForm";
 import accountForm from "./modules/accountForm";
+import makeMap from "./modules/map";
 
 $(document).foundation();
 
 loginForm();
 accountForm();
+makeMap($("#map"));
---
+++ public/javascripts/modules/map.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { $ } from "./bling";
+
+const mapOptions = {
+  center: { lat: 43.2, lng: -79.8 },
+  zoom: 10
+};
+
+function loadEvents() {
+
+}
+
+function makeMap(mapDiv) {
+  if (!mapDiv) return;
+}
+
+export default makeMap;
