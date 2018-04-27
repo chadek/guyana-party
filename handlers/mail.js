b1b47c8da65d5240ec73512d4ff970ad1f60ae27
spidergon <spidergon@gmail.com>
2018-04-27 13:00:45 -0300

    nodemailer using gmail service

--- handlers/mail.js
+++ handlers/mail.js
@@ -5,8 +5,7 @@ const htmlToText = require("html-to-text");
 const { promisify } = require("es6-promisify");
 
 const transport = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
+  service: process.env.MAIL_SERVICE,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS
