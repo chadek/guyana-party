e584c291d627ae9564d3b4b656c49c922a8ea5ac
spidergon <spidergon@gmail.com>
2018-05-05 22:31:26 -0300

    cleaning + footer correction

--- public/javascripts/modules/geolocation.js
+++ public/javascripts/modules/geolocation.js
@@ -1,12 +1,12 @@
+import dompurify from "dompurify";
 import { B } from "./bling";
 import { axiosGet } from "./utils";
-import dompurify from "dompurify";
 
 function geolocate() {
   navigator.geolocation.getCurrentPosition(pos => {
     // get town and postcode form nominatim
     axiosGet(
-      `http://nominatim.openstreetmap.org/reverse?format=json&lon=${pos.coords.longitude}&lat=${pos.coords.latitude}`,
+      `https://nominatim.openstreetmap.org/reverse?format=json&lon=${pos.coords.longitude}&lat=${pos.coords.latitude}`,
       data => {
         const text = dompurify.sanitize(
           `Autour de <strong style='color:#527fdc;'>${data.address.town} ${data.address.postcode}</strong>`
