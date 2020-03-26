9e95b2f92ded0615c2e4fd778712329e53fb8218
Christopher Servius <spidergon@gmail.com>
2020-03-26 18:41:34 -0300

    updates, corrections and cleaning

---
+++ client/.eslintrc.js
@@ -0,0 +1,107 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    jquery: true,
+    jest: true
+  },
+  extends: ['alloy', 'alloy/react'],
+  plugins: ['html', 'react', 'react-hooks', 'prettier'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        arrowParens: 'avoid',
+        printWidth: 120,
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'none'
+      }
+    ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-alert': 0,
+    'no-await-in-loop': 0,
+    'no-return-assign': ['error', 'except-parens'],
+    'no-restricted-syntax': [2, 'ForInStatement', 'LabeledStatement', 'WithStatement'],
+    'no-unused-vars': [
+      1,
+      {
+        ignoreSiblings: true,
+        argsIgnorePattern: 'res|next|^err'
+      }
+    ],
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'all'
+      }
+    ],
+    'arrow-body-style': [2, 'as-needed'],
+    'no-unused-expressions': [
+      2,
+      {
+        allowTaggedTemplates: true
+      }
+    ],
+    'no-param-reassign': [
+      2,
+      {
+        props: false
+      }
+    ],
+    radix: 0,
+    'no-shadow': [
+      2,
+      {
+        hoist: 'all',
+        allow: ['resolve', 'reject', 'done', 'next', 'err', 'error']
+      }
+    ],
+    quotes: [
+      2,
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true
+      }
+    ],
+    'import/prefer-default-export': 0,
+    import: 0,
+    'func-names': 0,
+    'space-before-function-paren': 0,
+    'comma-dangle': 0,
+    'max-len': 0,
+    'import/extensions': 0,
+    'no-underscore-dangle': 0,
+    'consistent-return': 0,
+    'react/display-name': 1,
+    'react/no-array-index-key': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/prefer-stateless-function': 0,
+    'react/forbid-prop-types': 0,
+    'react/no-unescaped-entities': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/require-default-props': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/jsx-sort-props': 'warn',
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx']
+      }
+    ],
+    'jsx-a11y/accessible-emoji': 0,
+    'jsx-a11y/href-no-hash': 'off'
+    // "jsx-a11y/label-has-for": 0,
+  },
+  settings: {
+    react: { version: 'detect' }
+  }
+}
